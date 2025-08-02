4b34721b3f336ec16b65bb12035ae5f74abbd004
Pankaj Bhaltilak <105781260+pankajbhaltilak@users.noreply.github.com>
2025-08-02 17:56:28 +0530

    updated branch dev

--- traders_portal/urls.py
+++ traders_portal/urls.py
@@ -6,7 +6,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Traders Portal -pankaj",
+      title="Traders Portal API",
       default_version='v1',
       description="API documentation for the Traders Portal Lite Backend",
       terms_of_service="https://www.google.com/policies/terms/",
